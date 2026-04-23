63647104be7c3a269477f57b1be7d80676c0adae
enal077 <zaenalajh4@gmail.com>
2026-04-23 19:59:31 +0700

    Implement traverse method for LinkedList
    Added a traverse method to display list contents.

--- 0051_singlelinkedlist.cpp
+++ 0051_singlelinkedlist.cpp
@@ -13,12 +13,14 @@ class LinkedList
 {
 private:
     Node *START;
+
 public:
     LinkedList()
     {
         START = NULL;
     }
-  void addNode()
+
+    void addNode()
     {
         int nim;
         cout << "\nMasukkan Nomor Mahasiswa: ";
@@ -54,7 +56,8 @@ public:
         nodeBaru->next = current;
         previous->next = nodeBaru;
     }
- bool listEmpty()
+
+    bool listEmpty()
     {
         return (START == NULL);
     }
@@ -88,6 +91,105 @@ public:
         delete current;
         return true;
     }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList kosong\n";
+            return;
+        }
+
+        cout << "\nData dalam list:\n";
+        Node *current = START;
+
+        while (current != NULL)
+        {
+            cout << current->noMhs << endl;
+            current = current->next;
+        }
+    }
+};
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char pilihan;
+
+    do
+    {
+        cout << "\n===== MENU =====\n";
+        cout << "1. Tambah data\n";
+        cout << "2. Hapus data\n";
+        cout << "3. Tampilkan data\n";
+        cout << "4. Cari data\n";
+        cout << "5. Keluar\n";
+        cout << "Pilihan (1-5): ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case '1':
+            mhs.addNode();
+            break;
+
+        case '2':
+            if (mhs.listEmpty())
+            {
+                cout << "\nList kosong\n";
+                break;
+            }
+
+            cout << "\nMasukkan NIM yang akan dihapus: ";
+            cin >> nim;
+
+            if (mhs.delNode(nim))
+                cout << "\nData berhasil dihapus\n";
+            else
+                cout << "\nData tidak ditemukan\n";
+            break;
+
+        case '3':
+            mhs.traverse();
+            break;
+
+        case '4':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << "\nList kosong\n";
+                break;
+            }
+
+            Node *prev, *curr;
+            cout << "\nMasukkan NIM yang dicari: ";
+            cin >> nim;
+
+            if (mhs.search(nim, prev, curr))
+            {
+                cout << "\nData ditemukan\n";
+                cout << "NIM: " << curr->noMhs << endl;
+            }
+            else
+            {
+                cout << "\nData tidak ditemukan\n";
+            }
+            break;
+        }
+
+        case '5':
+            cout << "\nKeluar program...\n";
+            break;
+
+        default:
+            cout << "\nPilihan tidak valid!\n";
+        }
+
+    } while (pilihan != '5');
+
+    return 0;
+}
 
     
    
