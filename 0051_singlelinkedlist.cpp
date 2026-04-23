2fc94f20b069725044f8dc750c07072ad8ad77a4
enal007 <zaenalajh4@gmail.com>
2026-04-23 18:40:07 +0700

    min

--- 0051_singlelinkedlist.cpp
+++ 0051_singlelinkedlist.cpp
@@ -9,5 +9,48 @@ public:
     node *next; 
 }
 
-class
-}
+class LinkedList
+{
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\n Duplikasikan noMhs tidak diijinkan\n";
+                return;
+            }
+            
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+
+        node *previous = START;
+        node *current = START;
+
+        while (current != NULL && nim > current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\n Duplikasikan noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
